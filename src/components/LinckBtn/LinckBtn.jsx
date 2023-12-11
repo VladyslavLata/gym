6a9a247d0acf9eb8515f53b54bb1656da2718ea7
VladyslavLata <vladlatahtc@gmail.com>
2023-12-11 16:11:11 +0200

    dd

--- src/components/LinckBtn/LinckBtn.jsx
+++ src/components/LinckBtn/LinckBtn.jsx
@@ -5,20 +5,15 @@ export const LinckBtn = ({ href, icon: Icon, name, currentLink }) => {
     <li className={styles[`${currentLink}LinkWrapp`]}>
       <a
         href={href}
-        className={styles.link}
+        // className={styles.link}
+        className={`${styles.link} ${styles[`${currentLink}Link`]}`}
         target="_blank"
         rel="noopener noreferrer"
         aria-label={`we are on ${name}`}
         onClick={(e) => e.currentTarget.blur()}
       >
         <div className={styles[`${currentLink}IconWrapp`]}>
-          <Icon
-            alt="icon"
-            width="100%"
-            height="100%"
-            fill="currentColor"
-            // stroke="ccurrentColoru"
-          />
+          <Icon alt="icon" width="100%" height="100%" fill="currentColor" />
         </div>
       </a>
     </li>
